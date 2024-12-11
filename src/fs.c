983b519ee1e6bbab680dae66e4c14c04ea7a2630
ensenginbaieer <lee.hsua@northeastern.edu>
2024-12-10 22:54:59 -0500

    Add function 'mov'

--- src/fs.c
+++ src/fs.c
@@ -54,10 +54,38 @@ void fs_delete(const char *name, fs_node_t *parent) {
     }
 }
 
-void fs_move(const char *src, const char *dest, fs_node_t *parent) {
-    fs_node_t *node = fs_find(src, parent);
-    if (node != NULL) {
-        strcpy(node->name, dest);
+void fs_move(const char* src, const char* dest, fs_node_t* parent) {
+    fs_node_t* node_to_move = fs_find(src, parent);
+    if (node_to_move == NULL) {
+        write("Source file/directory not found.\n", 32);
+        return;
+    }
+
+    // Find destination directory
+    fs_node_t* dest_dir = fs_find(dest, parent);
+    if (dest_dir == NULL || !dest_dir->is_directory) {
+        write("Destination must be a valid directory.\n", 36);
+        return;
+    }
+
+    // Remove node from current parent's children
+    for (unsigned int i = 0; i < parent->num_children; i++) {
+        if (parent->children[i] == node_to_move) {
+            for (unsigned int j = i; j < parent->num_children - 1; j++) {
+                parent->children[j] = parent->children[j + 1];
+            }
+            parent->num_children--;
+            break;
+        }
+    }
+
+    // Add node to destination directory
+    if (dest_dir->num_children < FS_MAX_FILES) {
+        dest_dir->children[dest_dir->num_children++] = node_to_move;
+        node_to_move->parent = dest_dir;
+    }
+    else {
+        write("Destination directory is full.\n", 30);
     }
 }
 
